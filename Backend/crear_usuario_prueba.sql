a2153f0ffd1b0e9e65441e8f03372f7bcad847e6
Mauro Pardo <maupardo03@gmail.com>
2025-12-31 17:27:27 -0300

    Removemos archivos a .gitignore

--- Backend/crear_usuario_prueba.sql
+++
@@ -1,9 +0,0 @@
--- Crear usuario de prueba para login
-INSERT INTO Usuario (nombre, email, contrasena) VALUES 
-('Administrador', 'admin@socks.com', '1234');
-
-INSERT INTO Usuario (nombre, email, contrasena) VALUES 
-('Juan Rodriguez', 'usuario_prueba@socks.com', '1234');
-
--- Ver usuarios creados
-SELECT * FROM Usuario;
